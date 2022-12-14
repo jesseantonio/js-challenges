e487b46faf0cf32fd8550efe7b1eda878758eabf
Jessé Antônio <jesseantonio1321@gmail.com>
2022-12-13 22:08:08 -0300

    # challenge #1

--- 1/index.js
+++ 1/index.js
@@ -1,22 +1,32 @@
 function drawCircle() {
     let canvasElem = document.querySelector("canvas");
-    canvasElem.addEventListener("click", function (e) {
+    canvasElem.addEventListener("click", (e) => {
         getMousePosition(canvasElem, e);
     });
 }
 
-
 function getMousePosition(canvas, event) {
     let rect = canvas.getBoundingClientRect();
-    let message = document.createElement('div');
+    let ball = document.createElement('div');
+    styleCircle(ball, event, rect)
     
-    message.style.cssText = "color: red";
-    message.style.width = "200"
-    message.style.height = "200"
-    message.innerHTML = "teste"
-    document.body.append(message);
+    var count = document.getElementById("holder").childElementCount;
+    console.log(count);
+    
+    document.getElementById("holder").appendChild(ball);
+}
 
+function styleCircle(ball, event, rect) {
+    ball.style.position = "absolute"
+    ball.style.width = "30px"
+    ball.style.height = "30px"
+    ball.style.borderRadius = "50%"
+    ball.style.backgroundColor = "red";
     let x = event.clientX - rect.left;
     let y = event.clientY - rect.top;
-    console.log(x, y)
+
+    ball.style.left = x + "px";
+    ball.style.top = y + "px";
+
+    return ball;
 }
--- 1/info.md
+++ 1/info.md
@@ -1,5 +1,5 @@
-## info
+## Challenge 01
 
 Build an application where each click inserts a circle in the exact position of the click.
 
-Also build an undo button, which will disappear with the circle added and a redo button that will add it back to the screen
+Also build an undo button, which will disappear with the circle added and a redo button that will add it back to the screen.
