14702d981f65962dc1db720a0f37909f06fff206
Jessé Antônio <jesseantonio1321@gmail.com>
2022-12-21 00:49:26 -0300

    #c2 - memory game

--- c2/index.js
+++ c2/index.js
@@ -1,10 +1,9 @@
+let arr;
+
 function createGame() {
     for (i = 0; i < 10; i++) {
         let card = document.createElement('div');
-        debugger
-        card.onclick= () => {
-            console.log(i + 1)
-        }
+        createMap()
         styleCard(card)
         document.getElementById("holder").appendChild(card);
     }
@@ -17,4 +16,34 @@ function styleCard(card) {
     card.style.marginBottom = "10px"
 
     return card
+}
+
+function createMap() {
+    let arr = [
+        { key: "", value: "banana" },
+        { key: "", value: "banana" },
+        { key: "", value: "apple" },
+        { key: "", value: "apple" },
+        { key: "", value: "pear" }, 
+        { key: "", value: "pear" }, 
+        { key: "", value: "watermelon" }, 
+        { key: "", value: "watermelon" }, 
+        { key: "", value: "grape" }, 
+        { key: "", value: "grape" }
+    ]      
+    randomizeMap(arr);
+    let map = new Map();
+    arr.forEach(obj => {
+        map.set(obj.key, obj.value)
+    })
+    console.log(map)
+    debugger
+    return map;
+}
+
+function randomizeMap(arr) {
+    debugger
+    arr.sort(() => Math.random() - 0.5)
+
+    return arr;
 }
