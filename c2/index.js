3dcee59d7ae00da7bc816024a1195cfdd5509070
Jessé Antônio <jesseantonio1321@gmail.com>
2022-12-22 00:22:06 -0300

    #c2 - memory game

--- c2/index.js
+++ c2/index.js
@@ -1,10 +1,11 @@
-let arr;
+let map;
 
 function createGame() {
+    createMap()
     for (i = 0; i < 10; i++) {
         let card = document.createElement('div');
-        createMap()
         styleCard(card)
+
         document.getElementById("holder").appendChild(card);
     }
 }
@@ -19,25 +20,18 @@ function styleCard(card) {
 }
 
 function createMap() {
-    let arr = [
-        { key: "", value: "banana" },
-        { key: "", value: "banana" },
-        { key: "", value: "apple" },
-        { key: "", value: "apple" },
-        { key: "", value: "pear" }, 
-        { key: "", value: "pear" }, 
-        { key: "", value: "watermelon" }, 
-        { key: "", value: "watermelon" }, 
-        { key: "", value: "grape" }, 
-        { key: "", value: "grape" }
-    ]      
-    randomizeMap(arr);
-    let map = new Map();
-    arr.forEach(obj => {
-        map.set(obj.key, obj.value)
-    })
-    console.log(map)
-    debugger
+    map = new Map();
+    map.set(1, "banana")
+    map.set(2, "banana")
+    map.set(3, "apple")
+    map.set(4, "apple")
+    map.set(5, "pear")
+    map.set(6, "pear")
+    map.set(7, "watermelon")
+    map.set(8, "watermelon")
+    map.set(9, "grape")
+    map.set(10, "grape")
+
     return map;
 }
 
@@ -46,4 +40,19 @@ function randomizeMap(arr) {
     arr.sort(() => Math.random() - 0.5)
 
     return arr;
+}
+
+function onSubmit() {
+    let val1 = parseInt(document.getElementById("value1").value);
+    let val2 = parseInt(document.getElementById("value2").value);
+    randomizeMap(map)
+    if (map.get(val1) === map.get(val2)) { 
+        map.delete(val1)
+        map.delete(val2)
+    }
+    if (map.size === 0) {
+        alert("Congrats!!!")
+    }
+
+    return map;
 }
